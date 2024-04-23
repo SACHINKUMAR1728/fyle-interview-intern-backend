cbf3be1a2b5d3d9d396697fabe0b5ecf154cf8cb
SACHINKUMAR1728 <sachinkumar_yadav@srmap.edu.in>
2024-04-23 15:56:30 +0530

    add grade tests passing

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -51,10 +51,13 @@ class Assignment(db.Model):
             assertions.assert_found(assignment, 'No assignment with this id was found')
             assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT,
                                     'only assignment in draft state can be edited')
+            assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+
 
             assignment.content = assignment_new.content
         else:
             assignment = assignment_new
+            assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
             db.session.add(assignment_new)
 
         db.session.flush()
@@ -66,19 +69,31 @@ class Assignment(db.Model):
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.student_id == auth_principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+        # assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT, 'only assignment in draft state can be submitted')
 
-        assignment.teacher_id = teacher_id
-        db.session.flush()
+        # assignment.teacher_id = teacher_id
+        # db.session.flush()
 
-        return assignment
+        # return assignment
+        if assignment.state == AssignmentStateEnum.SUBMITTED:
+            assertions.assert_valid(False, 'only a draft assignment can be submitted')
+        else:
+            assignment.teacher_id = teacher_id
+            assignment.state = AssignmentStateEnum.SUBMITTED
+            db.session.flush()
+            return assignment
 
 
     @classmethod
     def mark_grade(cls, _id, grade, auth_principal: AuthPrincipal):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
-        assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
-
+        assertions.assert_valid(grade is not None and grade in GradeEnum, 'assignment with empty grade cannot be graded')
+        assertions.assert_valid(assignment.state != AssignmentStateEnum.DRAFT, 'assignment in draft state cannot be graded')
+        if not auth_principal.principal_id:
+            assertions.assert_valid(assignment.teacher_id == auth_principal.teacher_id, 'This assignment belongs to some other teacher')
+            assertions.assert_valid(assignment.state != AssignmentStateEnum.GRADED, 'assignment is already graded')
+        
         assignment.grade = grade
         assignment.state = AssignmentStateEnum.GRADED
         db.session.flush()
